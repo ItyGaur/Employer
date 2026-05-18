698e95dea674b21eda550b25e18d2e4cf06bc45f
Ity Gaur <itygaur@gmail.com>
2026-05-18 22:08:46 +0530

    second commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"  // ← direct link
+    baseURL: "https://employer-wnd4.onrender.com"  // ← direct link
 });
 
 API.interceptors.request.use((req) => {
